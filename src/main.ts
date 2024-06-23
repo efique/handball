3db03062197fefd0490e5459f8c1f000e1177195
Valentin <valentin.barriol@hotmail.fr>
2024-06-23 19:14:47 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.PORT);
