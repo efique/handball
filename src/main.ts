a310e20ec38cd97188bc40a126f7ff98f4c479ff
Valentin <valentin.barriol@hotmail.fr>
2024-06-25 15:32:28 +0200

    cookies

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.use(cookieParser());
